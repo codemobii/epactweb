eff6a2300aa0b8b529231d2a99866cfa15522103
John Chimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2021-07-01 14:46:05 +0100

    Update api.util.js

--- utils/api.util.js
+++ utils/api.util.js
@@ -1,5 +1,7 @@
 export function getStrapiURL(path = "") {
-  return `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"}${path}`;
+  return `${
+    process.env.NEXT_PUBLIC_API_URL || "https://epact-api.herokuapp.com"
+  }${path}`;
 }
 
 // Helper to make GET requests to Strapi
