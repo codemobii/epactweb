75cf0ad36bf77230b0c3f8653cb9d248375ea852
John Chimaobi <45017704+ijelechimaobi@users.noreply.github.com>
2021-07-09 16:27:56 +0100

    Update main.layout.js

--- components/layouts/main.layout.js
+++ components/layouts/main.layout.js
@@ -3,15 +3,19 @@ import { Box } from "@chakra-ui/layout";
 import Head from "next/head";
 import Router from "next/router";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { GlobalContext } from "../../pages/_app";
+import { getStrapiMedia } from "../../utils/media.util";
 
 import FooterLayout from "./footer.layout";
 import MainHeader from "./header.layout";
 import Loader from "./loader.layout";
 import MainBottombarLayout from "./mainbottom.bar";
 
-export default function MainLayout({ children, title = "Home", fullSeo = {} }) {
+export default function MainLayout({ children, title = "Home" }) {
   const [loading, setLoading] = useState(false);
+
+  const global = useContext(GlobalContext);
 
   Router.events.on("routeChangeStart", (url) => {
     console.log(`Loading: ${url}`);
@@ -25,30 +29,11 @@ export default function MainLayout({ children, title = "Home", fullSeo = {} }) {
       <Head>
         <title>ePact :: {title}</title>
 
-        {fullSeo.metaTitle && (
-          <>
-            <meta property="og:title" content={fullSeo.metaTitle} />
-            <meta name="twitter:title" content={fullSeo.metaTitle} />
-          </>
-        )}
-        {fullSeo.metaDescription && (
-          <>
-            <meta name="description" content={fullSeo.metaDescription} />
-            <meta property="og:description" content={fullSeo.metaDescription} />
-            <meta
-              name="twitter:description"
-              content={fullSeo.metaDescription}
-            />
-          </>
-        )}
-        {fullSeo.shareImage && (
-          <>
-            <meta property="og:image" content={fullSeo.shareImage} />
-            <meta name="twitter:image" content={fullSeo.shareImage} />
-            <meta name="image" content={fullSeo.shareImage} />
-          </>
-        )}
-        {fullSeo.article && <meta property="og:type" content="article" />}
+        <meta property="og:title" content={`ePact :: ${title}`} />
+        <meta name="twitter:title" content={`ePact :: ${title}`} />
+        <meta property="og:image" content={getStrapiMedia(global.seo_image)} />
+        <meta name="twitter:image" content={getStrapiMedia(global.seo_image)} />
+        <meta name="image" content={getStrapiMedia(global.seo_image)} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
